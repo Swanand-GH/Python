9b4c8792050433f6a05906f7a1882afacf3f32b6
Swanand <swanand@users.noreply.github.com>
2026-08-08 17:56:59 +0530

    Add Q4.py solution for largest and smallest values

---
+++ Q4.py
@@ -0,0 +1,11 @@
+#4. Identify the largest and smallest numerical values within a provided list
+#Given Input: Data: [45, 12, 89, 2, 67]
+
+
+data = [45, 12, 89, 2, 67]
+
+largest = max(data)
+print("Largest value:", largest)
+
+smallest = min(data)
+print("Smallest value:", smallest)
