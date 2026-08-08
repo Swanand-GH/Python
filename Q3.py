c1c4a57239c4cd98ceaa6eaaf10356429794d8f5
Swanand <swanand@users.noreply.github.com>
2026-08-08 17:53:05 +0530

    Add Q3.py solution for sum and average

---
+++ Q3.py
@@ -0,0 +1,11 @@
+#3. Calculate the total sum of all integers in a list and find the arithmetic mean (average)
+#Given Input: Numbers: [10, 20, 30, 40, 50]
+
+
+numbers = [10, 20, 30, 40, 50]
+
+total = sum(numbers)
+print("Sum:", total)
+
+average = total / len(numbers)
+print("Average:", average)
