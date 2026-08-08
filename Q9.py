8f08d1798ad8ca895418d7016ad82bac9bcdff94
Swanand <swanand@users.noreply.github.com>
2026-08-08 18:18:11 +0530

    Add Q9.py solution for checking empty set

---
+++ Q9.py
@@ -0,0 +1,10 @@
+#9. Check whether a set is empty using conditional logic, and print an appropriate message
+#Given Input: data = set()
+
+
+data = set()
+
+if not data:
+    print("The set is empty")
+else:
+    print("The set has elements")
