c8eaf143819702bc920c6a0f490842349707fc2c
Swanand <swanand@users.noreply.github.com>
2026-08-08 19:02:24 +0530

    Add Q20.py solution for joining tuple of characters

---
+++ Q20.py
@@ -0,0 +1,8 @@
+#20. Convert a tuple of characters into a single joined string
+#Given Input: chars = ('a', 'b', 'c')
+
+
+chars = ('a', 'b', 'c')
+
+joined_string = ''.join(chars)
+print("Joined string:", joined_string)
