feb370ed9621301c83380bfac3f78d816761abf5
Swanand <swanand@users.noreply.github.com>
2026-08-08 18:59:32 +0530

    Add Q19.py solution for reversing a tuple

---
+++ Q19.py
@@ -0,0 +1,8 @@
+#19. Reverse the order of elements in a tuple
+#Given Input: items = (1, 2, 3, 4, 5)
+
+
+items = (1, 2, 3, 4, 5)
+
+reversed_tuple = items[::-1]
+print("Reversed tuple:", reversed_tuple)
